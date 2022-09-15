547cef09c302098e2786b348f4d4d53420bedb7b
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2022-09-14 20:19:09 -0700

    featureAddedidsandExpandedReesultsPage

--- assets/js/resultsPage.js
+++ assets/js/resultsPage.js
@@ -1,7 +1,25 @@
-$.ajax ({
-url:"https://api.nasa.gov/planetary/apod?api_key=QMAeSLw9LphOgRCAK702ASasb6X8HaxCufGsvHaw",
-success: function(whatyougot){document.getElementBy id("img").innerHTML="imgsrc="+whatyougot.url+"style=width:100%;'/>";
- document.getElementsByTagName(p).innerHTML="By"-whatyougot.copyright;
- document.getEelementByld("title*).innerHTML=whatyougot.title;
- document.getElementByld("explanation*).innerHTIML=whatyougot.explanation;
-]}):
+function getApi() {
+
+var requestURL = ("https://api.nasa.gov/planetary/apod?api_key=QMAeSLw9LphOgRCAK702ASasb6X8HaxCufGsvHaw")
+var searchbutton = ("document.blahblach")
+fetch (requestURL)
+.then(function(result) {return result.json();})
+// Loopy Thing  
+.then (function (data) {console.log(data)
+for (var i=0; i < data.length; i++) {
+    var bacsRow = document.getElementbyid('Name')
+    var Data = document.getElementbyid ('Metadata1')
+    var Data = document.getElementById('Metadata2')
+    var data = document.getElementbyid('source')
+    var tableData = document.getElementbyid ('Metadata1')
+    var tableData = document.getElementById('Metadata2')
+}   
+{ //Chang the links??
+    link.textContent = data[i].html_url;
+    link.href = datat[i].html_url;
+
+     //Append stuff
+}});
+}
+
+searchbutton.AddEventListener('click', getApi)
