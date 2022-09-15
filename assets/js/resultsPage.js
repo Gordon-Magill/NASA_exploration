b4df01a17fafc99dbd309511e3f6f2264c32acea
Gordon-Magill <gordon.magill@gmail.com>
2022-09-14 21:08:05 -0700

    Minor changes to API function

--- assets/js/resultsPage.js
+++ assets/js/resultsPage.js
@@ -1,31 +1,35 @@
+// $(".ui.labeled.icon.sidebar").sidebar("toggle").transition("slide left");
 
-$('.ui.labeled.icon.sidebar')
-  .sidebar('toggle')
-  .transition('slide left')
-;
+//   var searchButton = "document.blahblach";
+// searchbutton.AddEventListener("click", getApi);
 
 function getApi() {
+  var requestURL = "https://images-api.nasa.gov/search?q=mars";
+//   var requestURL = "https://images-api.nasa.gov/search?api_key=QMAeSLw9LphOgRCAK702ASasb6X8HaxCufGsvHaw&q=mars";
 
-var requestURL = ("https://api.nasa.gov/planetary/apod?api_key=QMAeSLw9LphOgRCAK702ASasb6X8HaxCufGsvHaw")
-var searchbutton = ("document.blahblach")
-fetch (requestURL)
-.then(function(result) {return result.json();})
-// Loopy Thing  
-.then (function (data) {console.log(data)
-for (var i=0; i < data.length; i++) {
-    var bacsRow = document.getElementbyid('Name')
-    var Data = document.getElementbyid ('Metadata1')
-    var Data = document.getElementById('Metadata2')
-    var data = document.getElementbyid('source')
-    var tableData = document.getElementbyid ('Metadata1')
-    var tableData = document.getElementById('Metadata2')
-}   
-{ //Chang the links??
-    link.textContent = data[i].html_url;
-    link.href = datat[i].html_url;
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    // Loopy Thing
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var bacsRow = document.getElementbyid("Name");
+        var Data = document.getElementbyid("Metadata1");
+        var Data = document.getElementById("Metadata2");
+        var data = document.getElementbyid("source");
+        var tableData = document.getElementbyid("Metadata1");
+        var tableData = document.getElementById("Metadata2");
+      }
+      {
+        //Chang the links??
+        link.textContent = data[i].html_url;
+        link.href = datat[i].html_url;
 
-     //Append stuff
-}});
+        //Append stuff
+      }
+    });
 }
 
-searchbutton.AddEventListener('click', getApi)
+getApi()
