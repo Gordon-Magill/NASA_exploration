b08ee279d105222fb381cd800ec807a37f9dc85d
Gordon-Magill <gordon.magill@gmail.com>
2022-09-15 20:29:22 -0700

    Search functionality working, need to handle CORS blocking

--- assets/js/resultsPage.js
+++ assets/js/resultsPage.js
@@ -1,30 +1,52 @@
 // $(".ui.labeled.icon.sidebar").sidebar("toggle").transition("slide left");
 // var apiKey = QMAeSLw9LphOgRCAK702ASasb6X8HaxCufGsvHaw
-//   var searchButton = "document.blahblach";
-// searchbutton.AddEventListener("click", getApi);
+var searchButton = $('#customSearchButton');
+searchButton.on("click", executeSearch);
 
+// Enables sidebar hiding and revealing
 // https://codepen.io/redshift7/pen/VaKmjq
-$('.ui.sidebar').sidebar({
-  context: $('.bottom.segment')
-})
-.sidebar('attach events', '.menu .item');
+$(".ui.sidebar")
+  .sidebar({
+    context: $(".bottom.segment"),
+  })
+  .sidebar("attach events", ".menu .item");
 
 // Setting up elements to drop in information from search result
-var nasaTitleEl = $('#nasaTitleEl');
-var nasaDescriptionEl = $('#nasaDescriptionEl');
-var nasaDateCreatedEl = $('#nasaDateCreatedEl');
-var nasaCenterEl = $('#nasaCenterEl');
-var nasaPhotoEl = $('#nasaPhotoEl');
-
-// Enables sidebar hiding
-// $('.customHeader').on('click',toggleSidebar)
-// function toggleSidebar(){
-//     $(".ui.labeled.icon.sidebar").sidebar("toggle");
-// }
+var nasaTitleEl = $("#nasaTitleEl");
+var nasaDescriptionEl = $("#nasaDescriptionEl");
+var nasaDateCreatedEl = $("#nasaDateCreatedEl");
+var nasaCenterEl = $("#nasaCenterEl");
+var nasaPhotoEl = $("#nasaPhotoEl");
 
 function executeSearch() {
-  var requestURL = "https://images-api.nasa.gov/search?q=mars";
+  // Input elements in the search bar
+  var descriptionField = $("#searchBarDescription");
+  var keywordsField = $("#searchBarKeywords");
+  var titleField = $("#searchBarTitle");
+  var startYearField = $("#searchBarStartYear");
+  var endYearField = $("#searchBarEndYear");
 
+  // Template URL with required query
+  var requestURL = `https://images-api.nasa.gov/search?q=${descriptionField.val()}`;
+
+  // Conditional statements that add in new query terms if they were added
+  if (keywordsField.val()) {
+    requestURL += `&keywords=${keywordsField.val()}`
+  }
+
+  if (titleField.val()) {
+    requestURL += `&title=${titleField.val()}`
+  }
+
+  if (startYearField.val()) {
+    requestURL += `&year_start=${startYearField.val()}`
+  }
+
+  if (endYearField.val()) {
+    requestURL += `&year_end=${endYearField.val()}`
+  }
+
+  // Fetching content based on the final request URL
   fetch(requestURL)
     .then(function (response) {
       return response.json();
@@ -37,30 +59,30 @@ function executeSearch() {
       var imgDescription = primaryResult.description;
       console.log(imgDescription);
       var imgTitle = primaryResult.title;
-      console.log(imgTitle)
+      console.log(imgTitle);
       var nasa_id = primaryResult.nasa_id;
-      console.log(nasa_id)
+      console.log(nasa_id);
 
-    //   Setting the page elements to the retrieved data
-      nasaTitleEl.text(imgTitle)
-      nasaDescriptionEl.text(imgDescription)
-      nasaDateCreatedEl.text(imgCreatedDate)
-    //   nasa
-      getImage(nasa_id)
+      //   Setting the page elements to the retrieved data
+      nasaTitleEl.text(imgTitle);
+      nasaDescriptionEl.text(imgDescription);
+      nasaDateCreatedEl.text(imgCreatedDate);
+      //   nasa
+      getImage(nasa_id);
     });
 }
 
 function getImage(nasa_id) {
-    fetch(`https://images-api.nasa.gov/asset/${nasa_id}`)
+  fetch(`https://images-api.nasa.gov/asset/${nasa_id}`)
     .then(function (response) {
-        return response.json()
+      return response.json();
     })
-    .then(function(data){
-        console.log(data)
-        var imgLink = data.collection.items[2].href; //Index 1 gets the "medium" image
-        console.log(imgLink)
-        nasaPhotoEl.attr('src',imgLink)
-    })
+    .then(function (data) {
+      console.log(data);
+      var imgLink = data.collection.items[2].href; //Index 1 gets the "medium" image
+      console.log(imgLink);
+      nasaPhotoEl.attr("src", imgLink);
+    });
 }
 
 executeSearch();
